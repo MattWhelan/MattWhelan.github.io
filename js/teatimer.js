132cb1cb2a4a02d308d94f04859f921477ee3784
Matt Whelan <matt@blacklogik.com>
2014-08-07 15:49:57 -0700

    Notifications, time in minutes.

--- js/teatimer.js
+++ js/teatimer.js
@@ -1,27 +1,50 @@
 $(function(){
   var INTERVAL = 1000;
-  var updateInterval = setInterval(update, INTERVAL);
-
   var start = Date.now(),
     stop;
 
   function update(){
     if (stop != null) {
-      var remaining = 1000 * (stop - start);
+      var remaining = Math.round((stop - Date.now())/1000);
       $("#timeLeft").text("T - " + remaining + "s");
     } else {
-      var elapsed = 1000 * (Date.now() - start);
+      var elapsed = Math.round((Date.now() - start)/1000);
       $("#timeLeft").text("elapsed:  " + elapsed + "s");
     }
   }
 
-  $("#duration").change(function(){
-    var duration = parseInt($(this).val(), 10);
-    stop = duration * 1000 + start;
+  function notify(){
+      $("#timeLeft").text("Done. " + new Date());
+      $title = $("title");
+      $title.text($title.text() + " - Done");
+      if (notifyAllowed) {
+        new Notification("Tea is done.");
+      }
+  }
+
+  var notifyAllowed = false;
+
+  if (window.Notification) {
+    if (Notification.permission === "granted") {
+      notifyAllowed = true;
+    } else if (Notification.permission !== 'denied') {
+      Notification.requestPermission(function (permission) {
+        if(permission == "granted") {
+          notifyAllowed = true;
+        }
+      });
+    }
+  }
+
+  var updateInterval = setInterval(update, INTERVAL);
+
+  $("#go").click(function(){
+    var duration = parseInt($("#duration").val(), 10);
+    stop = duration * 1000 * 60 + start;
 
     setTimeout(function(){
       clearInterval(updateInterval);
-      $("#timeLeft").text("Done.");
-    });
+      notify();
+    }, stop - Date.now());
   });
 });
